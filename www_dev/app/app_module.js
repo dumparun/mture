076d5ee1a753966eb583cadd5d892d76d377ad14
Arun <arun@dumparun.info>
2015-06-24 22:34:39 -0700

    seperated out app modules are routes for better management

---
+++ www_dev/app/app_module.js
@@ -0,0 +1,32 @@
+var mtureApp = angular.module('mtureApp', [
+        'ionic', 'ngCordova', 'ngMessages', 'loginApp', 'homeApp', 'workflowApp'
+]);
+
+angular.module('loginApp', []);
+
+angular.module('homeApp', []);
+
+angular.module('workflowApp', []);
+
+mtureApp.run([
+        '$ionicPlatform', '$cordovaSplashscreen', function($ionicPlatform, $cordovaSplashscreen) {
+
+	        $ionicPlatform.ready(function() {
+
+		        // Hide the accessory bar by default (remove this to
+		        // show the accessory bar above the keyboard
+		        // for form inputs)
+		        if (window.cordova && window.cordova.plugins.Keyboard) {
+			        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		        }
+		        if (window.StatusBar) {
+			        // org.apache.cordova.statusbar required
+			        StatusBar.styleDefault();
+		        }
+		        setTimeout(function() {
+
+			        $cordovaSplashscreen.hide()
+		        }, 3000)
+	        });
+        }
+]);
--- www_dev/app/app_routes.js
+++ www_dev/app/app_routes.js
@@ -1,35 +1,4 @@
-var mtureApp = angular.module('mtureApp', [
-        'ionic', 'ngCordova', 'ngMessages', 'loginApp', 'homeApp', 'workflowApp'
-]);
-
-angular.module('loginApp', []);
-
-angular.module('homeApp', []);
-
-angular.module('workflowApp', []);
-
-mtureApp.run([
-        '$ionicPlatform', '$cordovaSplashscreen', function($ionicPlatform, $cordovaSplashscreen) {
-
-	        $ionicPlatform.ready(function() {
-
-		        // Hide the accessory bar by default (remove this to
-		        // show the accessory bar above the keyboard
-		        // for form inputs)
-		        if (window.cordova && window.cordova.plugins.Keyboard) {
-			        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-		        }
-		        if (window.StatusBar) {
-			        // org.apache.cordova.statusbar required
-			        StatusBar.styleDefault();
-		        }
-		        setTimeout(function() {
-
-			        $cordovaSplashscreen.hide()
-		        }, 3000)
-	        });
-        }
-])
+angular.module('mtureApp')
 
 .config([
         '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
