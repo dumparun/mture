d23349d85605c20193898fd847133c46abb80ba5
Arun <arun@dumparun.info>
2015-06-28 11:55:42 -0700

    Alert message handling Directive added to handle alerts

---
+++ www_dev/app/app_directive.js
@@ -0,0 +1,23 @@
+angular
+        .module('mtureApp')
+        
+        .directive(
+                'mtAlert',
+                [
+                        '$timeout',
+                        function($timeout) {
+
+	                        return {
+	                            restrict : 'AE',
+	                            replace : true,
+	                            template : '<div  ng-hide = "angular.isUndefined(alert)" class={{alert.type}}>{{alert.message}}</div>',
+	                            link : function(scope, elem, attrs) {
+
+		                            $timeout(function() {
+			                            elem.remove();
+		                            }, 5000);
+		                            
+	                            },
+	                        };
+                        }
+                ]);
--- www_dev/app/home/homeController.js
+++ www_dev/app/home/homeController.js
@@ -3,6 +3,11 @@ angular.module('homeApp')
 .controller('HomeController', [
         '$scope', 'HomeDataService', function($scope, HomeDataService) {
 
+        	if(HomeDataService.getStatus().getStatusCode() == 0){
+        		$scope.alert = {};
+                $scope.alert.type = "success";
+                $scope.alert.message = "Successful Login";
+        	}
 	        $scope.entriesList = HomeDataService.getEntryList();
         }
 ])
